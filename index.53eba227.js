397be904e7ff83623cf4246862004fd85340b8e9
aleksandra-zablocka <aleksandra-zablocka@o2.pl>
2023-05-27 14:56:08 +0000

    Deploying to gh-pages from @ aleksandra-zablocka/goit-js-hw-11@335464745d2766c6381650b50c200a002ed20bd6 🚀

--- index.53eba227.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("#search-form"),document.querySelector("#search-form > input");
-//# sourceMappingURL=index.53eba227.js.map
--- index.ce7009af.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("#search-form"),document.querySelector("#search-form > input");
-//# sourceMappingURL=index.ce7009af.js.map
