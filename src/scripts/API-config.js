0264541c427790ab85cfe267f142c994e8832a07
Aleksandra Zabłocka <aleksandra-zablocka@o2.pl>
2023-05-29 14:51:27 +0200

    corrected api path

--- src/scripts/API-config.js
+++ src/scripts/API-config.js
@@ -1,4 +1,4 @@
-export const API_PATH = 'https://pixabay.com/api';
+export const API_PATH = 'https://pixabay.com/api/';
 export const API_KEY = '36802043-6369625f376675122720202cd';
 
 export const DEFAULT_PARAMS = {
