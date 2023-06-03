540d427eca1b1aaf6292a0fd14a1ba9ce11b0cca
Aleksandra Zabłocka <aleksandra-zablocka@o2.pl>
2023-06-03 10:27:09 +0200

    added timeout

--- src/scripts/handlers.js
+++ src/scripts/handlers.js
@@ -1,4 +1,5 @@
 import { loadPhotos } from './ui.js';
+let scrolling = false;
 
 export async function searchForPhotos(e) {
   e.preventDefault();
@@ -8,11 +9,22 @@ export async function searchForPhotos(e) {
 }
 
 export async function scrollHandler() {
+  if (scrolling) {
+    return
+  }
   const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
   if (scrollTop + clientHeight >= scrollHeight) {
     const searchForm = document.querySelector('#searchPhotosForm');
     const page = parseInt(searchForm.page.value);
     searchForm.page.value = page + 1;
+
+    scrolling = true;
+
     await loadPhotos({ q: searchForm.q.value, page: searchForm.page.value });
+    scrolling = false;
+
+    setTimeout(() => {
+      scrolling = false;
+    }, 1000)
   }
 }
--- src/scripts/pixabay.js
+++ src/scripts/pixabay.js
@@ -1,3 +1,51 @@
+import { API_PATH, DEFAULT_PARAMS } from './API-config.js';
+import Notiflix from 'notiflix';
+
+export default async function pingPixabay({ q = '', page = '1' }) {
+  try {
+    const queryString = new URLSearchParams({
+      ...DEFAULT_PARAMS,
+      page,
+      q,
+    });
+
+    const response = await fetch(`${API_PATH}?${queryString}`);
+    if (!response.ok) {
+      if (response.status === 400) {
+        return [];
+      }
+      return { error: response.status };
+    }
+    if (q === '') {
+      return [];
+    }
+    const { hits: photos, totalHits } = await response.json();
+
+    if (page !== '1' && photos.length === 0) {
+      Notiflix.Notify.failure(
+        "We're sorry, but you've reached the end of search results."
+      );
+      return;
+    }
+
+    if (photos.length === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+
+    if (page === '1' && q !== '') {
+      Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+    }
+
+    return photos;
+  } catch (e) {
+    return { error: e.toString() };
+  }
+}
+
+
 // import axios from 'axios';
 // import { API_PATH, DEFAULT_PARAMS } from './API-config.js';
 // import Notiflix from 'notiflix';
@@ -45,51 +93,3 @@
 //     return { error: e.toString() };
 //   }
 // }
-
-
-import { API_PATH, DEFAULT_PARAMS } from './API-config.js';
-import Notiflix from 'notiflix';
-
-export default async function pingPixabay({ q = '', page = '1' }) {
-  try {
-    const queryString = new URLSearchParams({
-      ...DEFAULT_PARAMS,
-      page,
-      q,
-    });
-
-    const response = await fetch(`${API_PATH}?${queryString}`);
-    if (!response.ok) {
-      if (response.status === 400) {
-        return [];
-      }
-      return { error: response.status };
-    }
-    if (q === '') {
-      return [];
-    }
-    const { hits: photos, totalHits } = await response.json();
-
-    if (page !== '1' && photos.length === 0) {
-      Notiflix.Notify.failure(
-        "We're sorry, but you've reached the end of search results."
-      );
-      return;
-    }
-
-    if (photos.length === 0) {
-      Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-      return;
-    }
-
-    if (page === '1' && q !== '') {
-      Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
-    }
-
-    return photos;
-  } catch (e) {
-    return { error: e.toString() };
-  }
-}
